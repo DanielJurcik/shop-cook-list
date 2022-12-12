bc3774862b1b50f333ac1b4b23ad09b978f6c9fa
Daniel Jurčík <dano.jurcik@gmail.com>
2022-12-12 19:42:17 +0100

    Code cleanup, replaced EventEmitters with Subject

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,25 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipes.model';
-import { RecipesService } from './recipes.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss'],
-  providers: [RecipesService]
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe : Recipe;
   
-  constructor(private recipesService : RecipesService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.recipesService.recipeSelected
-      .subscribe(
-        (recipe : Recipe) => {
-          this.selectedRecipe = recipe;
-        }
-      );
   }
 
 }
--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,11 +1,10 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipes.model";
 @Injectable()
 export class RecipesService{
-    recipeSelected = new EventEmitter<Recipe>();
-
     private recipes : Recipe[] = [
         new Recipe(
             1,
